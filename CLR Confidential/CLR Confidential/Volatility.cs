55e5e30d2e3698cc70a7f458d46575aa7baeac21
itowlson <github@hestia.cc>
2014-09-12 07:48:31 +1200

    Show Volatile.Read for volatile collection members

---
+++ CLR Confidential/CLR Confidential/Volatility.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CLR_Confidential
+{
+    public class Volatility
+    {
+        private volatile int _myVolatileInt = 123;
+        private int[] _myVastCollectionOfVolatileInts = new int[1000];
+
+        public int Read()
+        {
+            return _myVolatileInt;
+        }
+
+        public int ReadFromCollection(int index)
+        {
+            return Volatile.Read(ref _myVastCollectionOfVolatileInts[index]);
+        }
+    }
+}
